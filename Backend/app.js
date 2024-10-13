19db8abc64ad9bf2e9c122485fb9bc1832415673
Vishal Pathak <Vishalpathak.er@gmail.com>
2024-10-13 15:59:25 +0530

    deployement

---
+++ Backend/app.js
@@ -0,0 +1,15 @@
+import express from "express";
+import { configDotenv } from "dotenv";
+
+const app = express();
+configDotenv();
+
+
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(process.env.PORT, () => {
+  console.log("Server is running on port 3000");
+});
