05af44fc88d765614e84557541b9541aa1856b03
Vishal Pathak <Vishalpathak.er@gmail.com>
2024-10-15 11:12:37 +0530

    bus

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.use("/api/users", authRouter);
 
 // Home route
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.json({ message: "Welcome to the authentication API" });
 });
 
 
